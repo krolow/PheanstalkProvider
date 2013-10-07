9053c181e7df424c9373f6fc07d22e02ff66816e
Vinícius Krolow <vkrolow@conrad-caine.com>
2013-10-07 15:40:41 -0300

    Initial structure

---
+++ src/Cobaia/Pheanstalk/Provider/Silex/PheanstalkProvider.php
@@ -0,0 +1,24 @@
+<?php
+use Silex\Application;
+use Silex\ServiceProviderInterface;    
+use Pheanstalk_Pheanstalk;
+use Pheanstalk_PheanstalkInterface;
+
+class PheanstalkProvider implements SilexProvider
+{
+    public function register(Application $app)
+    {
+        $app['pheanstalk'] = $app->share(function () use ($app) {
+            $server = isset($app['pheanstalk.server']) ? $app['pheanstalk.server'] : '127.0.0.1';
+            $port = isset($app['pheanstalk.port']) ? $app['pheanstalk.port'] : Pheanstalk_PheanstalkInterface::DEFAULT_PORT;
+            $timeout = isset($app['pheanstalk.timeout']) ? $app['pheanstalk.timeout'] : 2;
+
+            return new Pheanstalk_Pheanstalk($server, $port, $timeout);
+        });
+    }
+
+    public function boot(Application $app)
+    {
+
+    }
+}
